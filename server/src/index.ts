aabbb42161576342d2487127feb82865ccd2a0b3
Patryk Pozniak <patryk.pozniak@reachplc.com>
2021-01-31 16:38:18 +0000

    Apply helmet and cors middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
 import express, { Application, Request, Response, NextFunction } from "express";
+import helmet from "helmet";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = 5000;
+
+app.use(helmet());
+app.use(cors());
 
 app.use("/", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: "Hello" });
